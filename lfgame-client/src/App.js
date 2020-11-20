ca0429ae199ebb49aaa4c31bcaaa2d81a02369a2
Etienne LC <etlecoent@gmail.com>
2020-11-20 22:25:05 +0000

    Code cleaning & added a 404 page

--- lfgame-client/src/App.js
+++ lfgame-client/src/App.js
@@ -6,47 +6,53 @@ import './App.scss';
 import useApplicationData from "./hooks/useApplication.js"
 
 const App = () => {
-  const {
-      state,
-      dispatch
-  } = useApplicationData();
-    const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
-));
-return (<div className="App" >
-  <NavBar />
-  <Router>
-    <Switch>
-      <Route exact path="/">
-        <GamesPage />      
-      </Route>
-
-      <Route path ="/register">
-        <h1>
-          This is the register page
-        </h1>
-      </Route>
-      
-      <Route path ="/login">
-        <h1>
-          This is the login page
-        </h1>
-      </Route>
-
-      <Route path="/profile">
-        <h1>
-          This is the profile page
-        </h1>
-      </Route>
-      
-
-      
-    </Switch>
-  </Router>
   
-  <h1> Users </h1>
-  <ul> {userList} </ul>
-</div >
-);
+  const {
+    state,
+    dispatch
+  } = useApplicationData();
+    
+  const userList = state.users.map((user) => (
+    <li key={user.id} > 
+      {user.first_name} {user.last_name}  {user.email} 
+    </li>
+  ));
+
+  return (
+    <div className="App" >
+      <NavBar />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <GamesPage />      
+          </Route>
+
+          <Route path ="/register">
+            <h1>
+              This is the register page
+            </h1>
+          </Route>
+          
+          <Route path ="/login">
+            <h1>
+              This is the login page
+            </h1>
+          </Route>
+
+          <Route path="/profile">
+            <h1>
+              This is the profile page
+            </h1>
+          </Route>
+
+          <Route path="*">
+            <h1>404 - Not Found</h1>
+          </Route>
+
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
