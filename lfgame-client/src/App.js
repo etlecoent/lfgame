1caf53f0b11e03181e0db0ac041f62fd80c86f88
Etienne LC <etlecoent@gmail.com>
2020-11-20 22:16:17 +0000

    Added the router logic

--- lfgame-client/src/App.js
+++ lfgame-client/src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import NavBar from './pages/Nav_bar/NavBar';
 import GamesPage from './pages/Games_page/GamesPage';
 import './App.scss';
@@ -12,7 +14,35 @@ const App = () => {
 ));
 return (<div className="App" >
   <NavBar />
-  <GamesPage />
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <GamesPage />      
+      </Route>
+
+      <Route path ="/register">
+        <h1>
+          This is the register page
+        </h1>
+      </Route>
+      
+      <Route path ="/login">
+        <h1>
+          This is the login page
+        </h1>
+      </Route>
+
+      <Route path="/profile">
+        <h1>
+          This is the profile page
+        </h1>
+      </Route>
+      
+
+      
+    </Switch>
+  </Router>
+  
   <h1> Users </h1>
   <ul> {userList} </ul>
 </div >
