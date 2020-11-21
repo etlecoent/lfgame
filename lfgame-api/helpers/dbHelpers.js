d95e473739c5d042961405b065d592d5be133785
Etienne LC <etlecoent@gmail.com>
2020-11-21 21:57:44 +0000

    Removed so useless helpers

--- lfgame-api/helpers/dbHelpers.js
+++ lfgame-api/helpers/dbHelpers.js
@@ -23,10 +23,10 @@ module.exports = (db) => {
           .catch((err) => err);
   }
 
-  const addUser = (firstName, lastName, email, password) => {
+  const addUser = (username, email, password) => {
       const query = {
-          text: `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *` ,
-          values: [firstName, lastName, email, password]
+          text: `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *` ,
+          values: [username, email, password]
       }
 
       return db.query(query)
@@ -34,24 +34,9 @@ module.exports = (db) => {
           .catch(err => err);
   }
 
-  const getUsersPosts = () => {
-      const query = {
-          text: `SELECT users.id as user_id, first_name, last_name, email, posts.id as post_id, title, content
-      FROM users
-      INNER JOIN posts
-      ON users.id = posts.user_id`
-      }
-
-      return db.query(query)
-          .then(result => result.rows)
-          .catch(err => err);
-
-  }
-
   return {
       getUsers,
       getUserByEmail,
-      addUser,
-      getUsersPosts
+      addUser
   };
 };
