77228977d1b153b08c195ddba7412d2485acc69e
Etienne LC <etlecoent@gmail.com>
2020-11-22 19:51:04 +0000

    Hashed the passwords

--- lfgame-api/routes/users.js
+++ lfgame-api/routes/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const jsonwebtoken = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const { getPostsByUsers } = require('../helpers/dataHelpers.js');
+
+
 
 module.exports = ({
     getUsers,
@@ -25,7 +28,7 @@ module.exports = ({
             email,
             password
         } = req.body;
-
+        
         getUserByEmail(email)
             .then(user => {
 
@@ -34,7 +37,8 @@ module.exports = ({
                         msg: 'Sorry, a user account with this email already exists'
                     });
                 } else {
-                    return addUser(username, email, password)
+                    const hashedPassword = bcrypt.hashSync(password, process.env.SALT_ROUNDS | 0)
+                    return addUser(username, email, hashedPassword);
                 }
             })
             .then(newUser => res.json({
@@ -59,7 +63,10 @@ module.exports = ({
             .then(user => {
 
                 if (user) {
-                    if (user.password === password) {
+                    console.log("password: " + password);
+                    console.log("user.password: " + user.password)
+                
+                    if (bcrypt.compareSync(password, user.password)) {
                         res.json({
                             username: user.username,
                             token: jsonwebtoken.sign({ username: user.username }, process.env.JWT_SECRET)
