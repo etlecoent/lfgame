b140d1d9eb722b56f70d8fc06e76421a53e6d49c
Etienne LC <etlecoent@gmail.com>
2020-11-22 01:23:13 +0000

    Added the created at in the db

--- lfgame-api/db/schema/01_users.sql
+++ lfgame-api/db/schema/01_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE users(
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
