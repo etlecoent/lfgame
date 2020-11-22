de80e8653a54cf8b623f68571dedf793eb0560d6
Etienne LC <etlecoent@gmail.com>
2020-11-22 00:45:33 +0000

    Added validations on the form

--- lfgame-client/src/pages/Login_page/LoginPage.js
+++ lfgame-client/src/pages/Login_page/LoginPage.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import axios from 'axios';
 
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { isEmail } from 'validator';
 
 import './LoginPage.scss'
 
@@ -11,6 +10,7 @@ const LoginPage = () => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
 
   const login = (email, password) => {
@@ -27,24 +27,38 @@ const LoginPage = () => {
         return response.data;
       });
   }
+  
+  const validate = () => {
+    if (!email) {
+      setMessage("Email cannot be blank");
+      return;
+    } else if (!isEmail(email)) {
+      setMessage(`${email} is not a valid email`);
+      return;
+    } else if (!password) {
+      setMessage("Password cannot be blank");
+      return;
+    } else {
+      setMessage("");
+      login(email, password)
+      // .then(grab user info, store in somewhere, I don't know where, token? We haven't even figured out authentication yet.)
+      // .then(redirect to games)
+      .catch(err => setMessage("Invalid email or password, please try again!"));
+    }
+  }
 
   return(
     <div>
       <header>Login</header>
+      {message && <div>{message}</div>}
       <form onSubmit={event => event.preventDefault}>
-
-        <Form.Group controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Email" onChange={event => setEmail(event.target.value)}/>
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" onChange={event => setPassword(event.target.value)}/>
-        </Form.Group>
-
-        <Button variant="primary" type="submit" onSubmit={() => login(email, password)}>
-          Submit
-        </Button>
-
+        <div>
+          <input type="email" placeholder="Email" value={email} onChange={event => setEmail(event.target.value)} />
+        </div>
+        <div>
+          <input type="password" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)} />
+        </div>
+        <button type='button' onClick={validate}>Submit</button>
       </form>
     </div>
   );
