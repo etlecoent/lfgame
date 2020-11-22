76206ec1baf319edf356cf9cb2d9244729095538
Etienne LC <etlecoent@gmail.com>
2020-11-22 01:25:07 +0000

    Changed the JSX of the page and added form validation

--- lfgame-client/src/pages/Register_page/RegisterPage.js
+++ lfgame-client/src/pages/Register_page/RegisterPage.js
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import axios from 'axios';
-
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { isEmail } from 'validator';
 
 import './RegisterPage.scss'
 
@@ -12,10 +10,9 @@ const RegisterPage = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-   
   const register = (username, email, password) => {
-    console.log("Jason rulez")
     return axios
       .post(API_URL, {
         username,
@@ -23,7 +20,6 @@ const RegisterPage = () => {
         password
       })
       .then(response => {
-        console.log("Jason sux")
         if (response.data.accessToken) {
           localStorage.setItem("user", JSON.stringify(response.data));
         }
@@ -31,27 +27,44 @@ const RegisterPage = () => {
       });
   }
 
+  const validate = () => {
+    if (!username) {
+      setMessage("Username cannot be blank");
+      return;
+    } else if (!email) {
+      setMessage("Email cannot be blank");
+      return;
+    } else if (!isEmail(email)) {
+      setMessage(`${email} is not a valid email`);
+      return;
+    } else if (!password) {
+      setMessage("Password cannot be blank");
+      return;
+    } else {
+      setMessage("");
+      register(username, email, password)
+      .then(res => console.log(res))
+      // .then(grab user info, store in somewhere, I don't know where, token? We haven't even figured out authentication yet.)
+      // .then(redirect to games)
+      .catch(err => setMessage("Email already exists, please use a different one!"));
+    }
+  }
+
   return(
     <div>
       <header>Register</header>
+      {message && <div>{message}</div>}
       <form onSubmit={event => event.preventDefault}>
-
-        <Form.Group controlId="formBasicUsername">
-          <Form.Control type="username" placeholder="Username" onChange={event => setUsername(event.target.value)}/>
-        </Form.Group>
-
-        <Form.Group controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Email" onChange={event => setEmail(event.target.value)}/>
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" onChange={event => setPassword(event.target.value)}/>
-        </Form.Group>
-
-        <Button variant="primary" type="submit" onSubmit={() => register(username, email, password)}>
-          Submit
-        </Button>
-
+        <div>
+          <input type="text" placeholder="Username" value={username} onChange={event => setUsername(event.target.value)} />
+        </div>
+        <div>
+          <input type="email" placeholder="Email" value={email} onChange={event => setEmail(event.target.value)} />
+        </div>
+        <div>
+          <input type="password" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)} />
+        </div>
+        <button type='button' onClick={validate}>Submit</button>
       </form>
     </div>
   );
