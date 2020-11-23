ae4c86650d9f9aa9b98cde5ada52480dcc0a2f21
Etienne LC <etlecoent@gmail.com>
2020-11-23 18:15:34 +0000

    Fixed a bug on socket not disconnecting

--- lfgame-api/routes/sessions.js
+++ lfgame-api/routes/sessions.js
@@ -8,6 +8,11 @@ const newUsers = [
   {id: 3, username: "user3"},
   {id: 4, username: "user4"}
 ]
+const upDatedUsers = [
+  {id: 2, username: "user2"},
+  {id: 3, username: "user3"},
+  {id: 4, username: "user4"}
+]
 
 module.exports = (io, {
   checkForSessionWithSpace
@@ -23,21 +28,35 @@ module.exports = (io, {
           .catch((err) => res.json({
               error: err.message
           }));
+    // if there is a session with space, add them to the session
+    // if there is not a session with space, create a new session with the game id and add them to that session
   });
-
 
   io.on("connection", (socket) => {
     
-    
+    // each socket has a unique id accessible with socket.id
+    // you can check the socket's rooms with socket.rooms
+    // { <socket.id>, "room1" }
+
     const { sessionId } = socket.handshake.query;
-    console.log("Connection to soket established");
+    console.log(`Socket with id: ${socket.id} connected`);
+    
     
     socket.join(sessionId);
 
     // // Join a conversation
-    
-    io.to(sessionId).emit("Hello", `Socket ${sessionId} joined`);
-    io.to(sessionId).emit("users", JSON.stringify(newUsers));  
+    io.to(sessionId).emit("Hello", `Room ${sessionId} joined`);
+    io.to(sessionId).emit("users", JSON.stringify(newUsers));
+
+    socket.on("disconnecting", (reason) => {
+      console.log(`${socket.id} has left ${socket.rooms}`) 
+      
+      socket.leave(sessionId);
+      io.to(sessionId).emit("user has left", JSON.stringify(upDatedUsers));
+      //removes the user from the session
+      //sends back the updated list of users in the session
+    });
+
   });
 
   return router;
