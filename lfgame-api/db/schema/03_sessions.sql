b1567e6effa1210673438e31fd53feb1b34b142e
Etienne LC <etlecoent@gmail.com>
2020-11-23 02:18:06 +0000

    Added sessions to the schema

--- lfgame-api/db/schema/03_sessions.sql
+++ lfgame-api/db/schema/03_sessions.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS sessions CASCADE;
 CREATE TABLE sessions(
     id SERIAL PRIMARY KEY NOT NULL,
     game_id INTEGER NOT NULL REFERENCES games(id) ON DELETE CASCADE,
-    play_level VARCHAR(255) NOT NULL,
+    -- play_level VARCHAR(255) NOT NULL, commented out for now, until we impliment this
     created_at VARCHAR(255) NOT NULL,
     population smallint DEFAULT 0,
     status boolean
