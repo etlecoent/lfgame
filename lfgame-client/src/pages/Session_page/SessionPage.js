1291e13e1205b3e1134c0d6698acfabd5eae8324
Etienne LC <etlecoent@gmail.com>
2020-11-23 19:45:35 +0000

    Added handling messages

--- lfgame-client/src/pages/Session_page/SessionPage.js
+++ lfgame-client/src/pages/Session_page/SessionPage.js
@@ -7,44 +7,58 @@ const SessionPage = (props) => {
   
   const NEW_USER_EVENT = "newUser"; // Name of the event
 
+  const messageExample = {
+    username: 'XSlayerX',
+    content: 'fast BOIZZZZZZZZZ'
+  }
+
   const [users, setUsers] = useState([]);
+  const [messages, setMessages] = useState([]);
   const socketRef = useRef();
+
+
+  const leaveSession = () => {
+    socketRef.current.disconnect();
+  };
+
+  const sendMessage = (username, content) => {
+    const outgoingMessage = {
+      username: 'XSlayerX',
+      content: 'fast BOIZZZZZZZZZ'
+    }
+    socketRef.current.emit("sending message", outgoingMessage)
+  } 
 
   useEffect(() => {
     
     socketRef.current = io({
-      path: '/socket.io',  
+      path: '/socket.io',
       query: {sessionId: 1},
       // auth: { token: "abc" }
     });
 
-    // Listens for incoming users
-    socketRef.current.on("Hello", (data) => {
-      console.log(data);
-    });
-
-    socketRef.current.on("users", (users) => {
-      console.log(users);
+    
+    socketRef.current.on("user has joined", (users) => {
+      setMessages(messages => [...messages, {username: "System", content: "User X has joined the channel"}])
+      
       const newUsers = JSON.parse(users);
       setUsers([...newUsers]);
     });
 
     socketRef.current.on("user has left", (users) => {
       console.log(users);
+
+      setMessages(messages => [...messages, {username: "System", content: "User X has left the channel"}])
+
       const newUsers = JSON.parse(users);
       setUsers([...newUsers]);
     });
 
-  }, []);
+    socketRef.current.on("incoming message", (message) => {
+      setMessages(messages => [...messages, message])
+    })
 
-  
-  // Destroys the socket reference
-  // when the connection is closed
-  
-  const leaveSession = () => {
-    socketRef.current.disconnect();
-  };
-  
+  }, []);
 
   return (
     <div>
@@ -59,8 +73,18 @@ const SessionPage = (props) => {
         {users.map(user => <li>{user.username}</li>)}
         {/* <GamerList users={users}/> */}
       </ul>
+      <h2>
+        Messages
+      </h2>
+      <ul>
+        {messages.map(message => <li>{message.content}</li>)}
+        {/* <GamerList users={users}/> */}
+      </ul>
       <button onClick={() => leaveSession()}>
         Leave session
+      </button>
+      <button onClick={() => sendMessage()}>
+        Send Message
       </button>
     </div>
   )
