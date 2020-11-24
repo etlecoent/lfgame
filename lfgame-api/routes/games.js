bbfeaba3a3aba4a27b3af4b10e8c5665b3b2384c
Etienne LC <etlecoent@gmail.com>
2020-11-24 03:31:03 +0000

    Created the get route for the games

---
+++ lfgame-api/routes/games.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const jsonwebtoken = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+
+module.exports = ({
+    getGames
+}) => {
+    
+  router.get("/", (req, res) => {
+    getGames()
+      .then((games) => res.json(games))
+      .catch((err) => res.json ({
+        error: err.message
+      }));
+  });
+
+  return router;
+};
