01183152e7a2f5d83803f12443561679618053dc
Etienne LC <etlecoent@gmail.com>
2020-11-24 03:28:27 +0000

    Changed the schema to make session chat dynamic

--- lfgame-api/db/schema/04_joined_sessions.sql
+++ lfgame-api/db/schema/04_joined_sessions.sql
@@ -2,5 +2,6 @@ DROP TABLE IF EXISTS joined_sessions CASCADE;
 CREATE TABLE joined_sessions(
     id SERIAL PRIMARY KEY NOT NULL,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    session_id INTEGER NOT NULL REFERENCES sessions(id) ON DELETE CASCADE
+    session_id INTEGER NOT NULL REFERENCES sessions(id) ON DELETE CASCADE,
+    in_session boolean default true
 );
