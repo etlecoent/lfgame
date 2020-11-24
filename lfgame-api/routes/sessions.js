3103a30c10459fe57e2972c8e8d3c399b95b77eb
Etienne LC <etlecoent@gmail.com>
2020-11-24 03:30:10 +0000

    Added the data to event emitters

--- lfgame-api/routes/sessions.js
+++ lfgame-api/routes/sessions.js
@@ -15,45 +15,82 @@ const upDatedUsers = [
 ]
 
 module.exports = (io, {
-  checkForSessionWithSpace
+  checkForSessionWithSpace,
+  addUserToAvailableSession,
+  createNewSession,
+  usersInSession,
+  removeUserFromSession
 }) => {
 
 
   /* Check for available sessions */
-  router.get('/', (req, res) => {
-    
-    checkForSessionWithSpace( 1 )
-          .then((session) => res.json(session))
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    const { gameID, userID } = req.body;
+    // req.userID
+    // req.gameID
+    // maybe this is how we'll get the shit
+    checkForSessionWithSpace(gameID)
+          .then((session) => {
+            // if there is a session with space, add them to the session
+            if (session) {
+              return addUserToAvailableSession(userID, session.id)
+
+              // then go to that session
+            } else {
+              // if there is not a session with space, create a new session with the game id and add them to that session
+              return createNewSession(gameID)
+              .then(newSession => {
+                return addUserToAvailableSession(userID, newSession.id)
+              })
+            }
+          })
+          // then go to that session
+          .then(joinedSession => {
+            res.json({session_id: joinedSession.session_id})
+          })
           .catch((err) => res.json({
               error: err.message
           }));
-    // if there is a session with space, add them to the session
-    // if there is not a session with space, create a new session with the game id and add them to that session
+
   });
 
   io.on("connection", (socket) => {
-    
+  
 
-    const { sessionId } = socket.handshake.query;
+
+    const { sessionId, username, userId } = socket.handshake.query;
+
     console.log(`${socket.id} has connected`);
     socket.sessionId = sessionId;
+    socket.username = username;
+    socket.userId = userId;
+    // socket.userId = userId;
     
+    // Adds the user to the according sessions
     socket.join(socket.sessionId);
 
-    io.to(sessionId).emit("user has joined", JSON.stringify(newUsers));
+    // Sends all the users inside the session to the newly joined user
+    usersInSession(socket.sessionId).then(res => {
+      io.to(socket.sessionId).emit("user has joined", res)  
+    });
+  
 
     socket.on("sending message", (message) => {
-      console.log("HIT");
       io.to(socket.sessionId).emit("incoming message", message);
     });
 
     socket.on("disconnecting", (reason) => {
       console.log(`${socket.id} has left ${socket.rooms}`) 
       
-      socket.leave(sessionId);
-      io.to(sessionId).emit("user has left", JSON.stringify(upDatedUsers));
       //removes the user from the session
       //sends back the updated list of users in the session
+      removeUserFromSession(socket.userId, socket.sessionId).then( res => {
+        usersInSession(socket.sessionId).then(updatedUsers => {
+          io.to(sessionId).emit("user has left", JSON.stringify(updatedUsers));
+          socket.leave(socket.sessionId);
+        });
+      })
     });
 
   });
