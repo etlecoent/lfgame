a53bccfa62eb37cae6fd5d000e6e6d94f849b9a0
Etienne LC <etlecoent@gmail.com>
2020-11-24 19:51:23 +0000

    Added pictures rendering on games lists

--- lfgame-client/src/pages/Session_page/GameInfos.js
+++ lfgame-client/src/pages/Session_page/GameInfos.js
@@ -8,7 +8,7 @@ const GameInfo = (props) => {
         {props.gameInfo.name}
       </header>
       <div>
-        <img className="gameImage" src={props.imageSource}></img>
+        <img className="gameImage" src={props.gameInfo.picture_url}></img>
       </div>
     </article>
   )
