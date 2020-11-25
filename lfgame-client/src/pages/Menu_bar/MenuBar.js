e41adb80df57310923950436b1e348401f4be964
Etienne LC <etlecoent@gmail.com>
2020-11-25 22:44:25 +0000

    Fixed className typo

--- lfgame-client/src/pages/Menu_bar/MenuBar.js
+++ lfgame-client/src/pages/Menu_bar/MenuBar.js
@@ -1,5 +1,5 @@
 import { Link, Redirect } from "react-router-dom";
-import DropDown from "react-bootstrap/DropDown";
+import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 
 import "./MenuBar.scss";
@@ -8,24 +8,20 @@ import "./MenuBar.scss";
 const MenuBar = (props) => {
 
   return (
-    <DropDown className="menuBar">
-      {/* <DropDown.Toggle key={"up"} drop={"up"} id="dropdown-button-drop-up">Menu</DropDown.Toggle> */}
-      {props.currentUser && 
+    <Dropdown className="menuBar">
+      
+    {props.currentUser && 
       <DropdownButton
         key={"up"}
         id={"dropdown-button-drop-up"}
         drop={"up"}
         title={"Menu"}
       >
-      {/* <DropDown.Menu className="menu"> */}
-          <DropDown.Item class="menuItem" href="/profile" >Profile</DropDown.Item>
-          {/* <Dropdown.Item href="/profile/edit">Edit Profile</Dropdown.Item> */}
-          <DropDown.Item class="menuItem" onClick={props.logout}>Logout</DropDown.Item>
-          {/* <Dropdown.Item href="/home">Home Page</Dropdown.Item> */}
-      {/* </DropDown.Menu> */}
-        
-      </DropdownButton> }
-  </DropDown>
+      <Dropdown.Item className="menuItem" href="/profile" >Profile</Dropdown.Item>
+      <Dropdown.Item className="menuItem" onClick={props.logout}>Logout</Dropdown.Item>
+      </DropdownButton>
+    }
+  </Dropdown>
   )
 }
 
