a1889a8f13335909f487c96ca9d10462888879e9
Etienne LC <etlecoent@gmail.com>
2020-11-25 19:12:24 +0000

    Finished the styling of the registerPage

--- lfgame-client/src/pages/Register_page/RegisterPage.js
+++ lfgame-client/src/pages/Register_page/RegisterPage.js
@@ -30,14 +30,14 @@ const RegisterPage = (props) => {
   }
 
   const validate = () => {
-    if (!username) {
-      setMessage("Username cannot be blank");
-      return;
-    } else if (!email) {
+    if (!email) {
       setMessage("Email cannot be blank");
       return;
     } else if (!isEmail(email)) {
       setMessage(`${email} is not a valid email`);
+      return;
+    } else if (!username) {
+      setMessage("Username cannot be blank");
       return;
     } else if (!password) {
       setMessage("Password cannot be blank");
@@ -51,26 +51,33 @@ const RegisterPage = (props) => {
 
   return(
     <section className="page">
-      <header>Register</header>
-      {message && <div>{message}</div>}
-      <form onSubmit={event => event.preventDefault()}>
-        <div>
-          <input type="text" placeholder="Username" value={username} onChange={event => setUsername(event.target.value)} />
+      <div id="registerPage">
+        <header>Register</header>
+          {message && <div className="alert alert-danger">{message}</div>}
+          <form onSubmit={event => event.preventDefault()}>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={email} onChange={event => setEmail(event.target.value)}/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputUsername">Username</label>
+              <input type="text" className="form-control" id="exampleInputUsername1" aria-describedby="usernameHelp" placeholder="Enter username" value={username} onChange={event => setUsername(event.target.value)}/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)}/>
+            </div>
+            <button type="button" className="btn btn-primary" onClick={validate}>Submit</button>
+          </form>
+        
+
+        <div className="loginButton">
+          <Link to="/login">
+            <button className ="btn btn-outline-primary">
+              Log in
+            </button>
+          </Link>
         </div>
-        <div>
-          <input type="email" placeholder="Email" value={email} onChange={event => setEmail(event.target.value)} />
-        </div>
-        <div>
-          <input type="password" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)} />
-        </div>
-        <button type='button' onClick={validate}>Submit</button>
-      </form>
-      <div>
-        <Link to="/login">
-          <button>
-            Have an account? Log in here
-          </button>
-        </Link>
       </div>
     </section>
   );
