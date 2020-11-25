5550e6cb2172f6b6535a1beae707bccab8ce2d7c
Jason Long <jason.long77@hotmail.com>
2020-11-25 03:44:10 +0000

    Fixed previous session list

--- lfgame-client/src/pages/Profile_page/PrevSessionsList.js
+++ lfgame-client/src/pages/Profile_page/PrevSessionsList.js
@@ -2,13 +2,16 @@ import PrevSessionItem from './PrevSessionItem';
 
 const PrevSessionsList = (props) => {
 
+  const currentUser = props.currentUser;
+
   return props.sessionsList.map(session => {
     return (
 
       <PrevSessionItem 
         key={session.id}
-        joinID={session.joinedid}
+        sessionID={session.sessionid}
         game={session.name}
+        currentUser={currentUser}
         
       
       
