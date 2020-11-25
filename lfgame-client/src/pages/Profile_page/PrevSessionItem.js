57f37754625d021f9fa53ed0ed86686a822552b5
Jason Long <jason.long77@hotmail.com>
2020-11-25 03:48:26 +0000

    Added userList which shows previous users from this session

--- lfgame-client/src/pages/Profile_page/PrevSessionItem.js
+++ lfgame-client/src/pages/Profile_page/PrevSessionItem.js
@@ -1,15 +1,44 @@
-const { default: PrevSessionsList } = require("./PrevSessionsList");
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 
 const PrevSessionItem = (props) => {
 
+  const currentUser = props.currentUser;
+  const sessionID = props.sessionID;
+  const [userList, setUserList] = useState([])
 
+  // useEffect(() => {
+  //   axios.get(`api/users/${currentUser.username}/${sessionID}`).then(res => {
+  //     console.log("Nonsense ", res)
+  //     setUserList(["Jason", "Jeremy"]);
+  //   })
+  // }, [])
+
+  const populatePeople = () => {
+    if (userList.length > 0) {
+      setUserList([]);
+    } else {
+      axios.get(`api/users/${currentUser.username}/${sessionID}`).then(res => {
+        console.log("Nonsense ", res.data[0])
+        const parsedList = res.data.map(user => user.username);
+        setUserList([...parsedList]);
+
+      })
+    }
+  }
+
+  
   return (
-    <div>
+    <div onClick={populatePeople}>
       <p>
         Game: {props.game}, 
-        Session ID: {props.joinID}
+        Session ID: {props.sessionID}, 
+        Players: 
       </p>
+      <ul>
+        {userList && userList.map(username => <li>{username}</li>)}
+      </ul>
     </div>
 
   )
