c11f4e45d55bc1df3911f17d7a342f2b936c50dd
Etienne LC <etlecoent@gmail.com>
2020-11-26 01:26:45 +0000

    Added a default profil picture on the user

--- lfgame-api/db/schema/01_users.sql
+++ lfgame-api/db/schema/01_users.sql
@@ -4,5 +4,6 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
+    image TEXT DEFAULT 'https://www.artifacting.com/blog/wp-content/uploads/2010/11/Spiderman-150x126.png',
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
