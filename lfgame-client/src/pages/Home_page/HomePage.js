8f0d1c00152e629c44ce2ec788c4a0ee6e2b487c
Etienne LC <etlecoent@gmail.com>
2020-11-27 19:34:44 +0000

    Added testomonies & about us on the home page

--- lfgame-client/src/pages/Home_page/HomePage.js
+++ lfgame-client/src/pages/Home_page/HomePage.js
@@ -16,11 +16,19 @@ const HomePage = (props) => {
           <header>Connect with gamers</header>
         </div>
         
-        <div id="about">
-          <p>
-            A place where gamers can look for groups to play games with. Perfect for people who just bought a game but have no one to play it with.
-          </p>
-        </div> 
+        <p id="about">
+          A place where gamers can look for groups to play games with. Perfect for people who just bought a game but have no one to play it with.
+        </p>
+
+        <span className="testimony">
+          XSlayerX: "Way easier to find Among Us sessions here"
+        </span>
+
+        <span className="testimony">
+          C-J: "I made so much friends thanks to this App!"
+        </span>
+
+
         <div className="buttons">
 
           <div className="registerButton">
