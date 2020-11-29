d893fa7cb7b44a732667da6e00b81551a0c08f5b
Jason Long <jason.long77@hotmail.com>
2020-11-29 23:17:11 +0000

    Added avatars for users in DB

--- lfgame-api/db/schema/01_users.sql
+++ lfgame-api/db/schema/01_users.sql
@@ -5,6 +5,6 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     steam_id VARCHAR(255),
-    image TEXT DEFAULT 'https://www.artifacting.com/blog/wp-content/uploads/2010/11/Spiderman-150x126.png',
+    image TEXT DEFAULT 'https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg',
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
