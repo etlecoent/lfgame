c032b85fd0ce89ca3aa4b9321c7c099fae392c41
Etienne LC <etlecoent@gmail.com>
2020-11-30 15:42:07 -0500

    Removed user Etienne for demo purposes

--- lfgame-api/db/seeds/01_users.sql
+++ lfgame-api/db/seeds/01_users.sql
@@ -3,7 +3,6 @@ users(username, email, password, image)
 VALUES
     ('Noah', 'noah@noah.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://media.discordapp.net/attachments/190252372054769664/782715463037222912/IMG_20201123_233814_631.jpg'),
     ('Jason', 'jason@jason.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://bloximages.chicago2.vip.townnews.com/thesouthern.com/content/tncms/assets/v3/editorial/1/f8/1f8051e7-42f8-5777-9b0d-73ef002fb023/5bb253a30e6e3.image.jpg'),
-    ('Etienne', 'etienne@etienne.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4a243a29-186e-46e2-b5fd-3581c92b8930/dcpsjkv-7ceeda17-eea3-48a4-8aea-a8a18d237cf7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNGEyNDNhMjktMTg2ZS00NmUyLWI1ZmQtMzU4MWM5MmI4OTMwXC9kY3Bzamt2LTdjZWVkYTE3LWVlYTMtNDhhNC04YWVhLWE4YTE4ZDIzN2NmNy5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.X_ruSWO32cIbPDCu-jqWRKAeqsGUZorE8FE_5xvNqo4'),
     ('Gazoo', 'gazoo@gazoo.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://media.discordapp.net/attachments/190252372054769664/782715463037222912/IMG_20201123_233814_631.jpg');
 
 INSERT INTO
