0b5dd059b3140acdbc088509446b610cf0aeac84
Jason Long <jason.long77@hotmail.com>
2020-12-02 22:07:21 +0000

    Added avatars for several users

--- lfgame-api/db/seeds/01_users.sql
+++ lfgame-api/db/seeds/01_users.sql
@@ -3,22 +3,23 @@ users(username, email, password, image)
 VALUES
     ('Noah', 'noah@noah.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://media.discordapp.net/attachments/190252372054769664/782715463037222912/IMG_20201123_233814_631.jpg'),
     ('Jason', 'jason@jason.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://bloximages.chicago2.vip.townnews.com/thesouthern.com/content/tncms/assets/v3/editorial/1/f8/1f8051e7-42f8-5777-9b0d-73ef002fb023/5bb253a30e6e3.image.jpg'),
+    ('Francis', 'francis@francis.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://i.imgur.com/EllOyhf.png'),
+    ('Chris', 'chris@chris.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://i.imgur.com/0hAwmXc.png'),
+    ('Rajdeep', 'rajdeep@rajdeep.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://i.imgur.com/HObB3ZW.png'),
+    ('Jess', 'jess@jess.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://i.imgur.com/b0EjCeQ.png'),
+    ('Sean', 'sean@sean.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://i.imgur.com/OjbJZ6w.png'),
+    ('Jacqueline', 'jacqueline@jacqueline.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://i.imgur.com/0AXkLuV.png'),
+    ('Jen', 'jen@jen.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://i.imgur.com/cgg88aY.png'),
     ('Gazoo', 'gazoo@gazoo.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6', 'https://media.discordapp.net/attachments/190252372054769664/782715463037222912/IMG_20201123_233814_631.jpg');
 
 INSERT INTO
 users(username, email, password)
 VALUES
-    ('Jess', 'jess@jess.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('David', 'david@david.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('Alex', 'alex@alex.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('Erik', 'erik@erik.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
-    ('Jen', 'jen@jen.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
-    ('Jacqueline', 'jacqueline@jacqueline.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('Daniel', 'daniel@daniel.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('Ian', 'ian@ian.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
-    ('Chris', 'chris@chris.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
-    ('Rajdeep', 'rajdeep@rajdeep.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
-    ('Sean', 'sean@sean.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('Vasily', 'vasily@vasily.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('Chetna', 'Chetna@Chetna.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('Janelle', 'janelle@janelle.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
@@ -26,5 +27,4 @@ VALUES
     ('Mad', 'mad@mad.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('Spiderman', 'spiderman@spiderman.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('Megaman', 'megaman@megaman.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
-    ('Francis', 'francis@francis.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6'),
     ('XSlayerX', 'slayer@slayer.com', '$2b$10$SQ0xAgiw64O6cvoHrfB5sehfIItqOArMWXbRT0FJu3JaAK/LEXOC6');
